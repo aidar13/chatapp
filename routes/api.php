d327f551d9ee86318a999dbcb32cd696d7d41520
Aidar <kanybek@spark.kz>
2024-04-18 04:55:53 +0500

    added get user chats filtered by message time

--- routes/api.php
+++ routes/api.php
@@ -5,29 +5,30 @@ use App\Http\Controllers\V1\ChatController;
 use App\Http\Controllers\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
-//Auth
-Route::post('/register', [AuthController::class, 'register'])
-    ->name('v1.auth.register');
+Route::middleware('throttle:10,1')->group(function () {
+    //Auth
+    Route::post('/register', [AuthController::class, 'register'])
+        ->name('v1.auth.register');
 
-Route::post('/token', [AuthController::class, 'token'])
-    ->name('v1.auth.token');
+    Route::post('/token', [AuthController::class, 'token'])
+        ->name('v1.auth.token');
 
+    Route::middleware('auth:sanctum')->group(function () {
+        //User
+        Route::get('/me', [UserController::class, 'me'])
+            ->name('v1.users.me');
 
-Route::middleware('auth:sanctum')->group(function () {
-    //User
-    Route::get('/me', [UserController::class, 'me'])
-        ->name('v1.users.me');
+        Route::get('/users', [UserController::class, 'index'])
+            ->name('v1.users.index');
 
-    Route::get('/users', [UserController::class, 'index'])
-        ->name('v1.users.index');
+        Route::get('/users/chats', [UserController::class, 'chats'])
+            ->name('v1.users.chats');
 
-    Route::get('/users/chats', [UserController::class, 'chats'])
-        ->name('v1.users.chats');
+        //Chat
+        Route::post('/chats', [ChatController::class, 'store'])
+            ->name('v1.chats.store');
 
-    //Chat
-    Route::post('/chats', [ChatController::class, 'store'])
-        ->name('v1.chats.store');
-
-    Route::post('/chats/{id}/message', [ChatController::class, 'sendMessage'])
-        ->name('v1.chats.send-message');
+        Route::post('/chats/{id}/message', [ChatController::class, 'sendMessage'])
+            ->name('v1.chats.send-message');
+    });
 });
